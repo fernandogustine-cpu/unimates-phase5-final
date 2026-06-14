4d84ff0962cc58d844a196083c47a7e08c12712d
fernandogustine-cpu <fernandogustine@gmail.com>
2026-06-14 21:19:06 +0200

    Update page.js

--- app/homework/page.js
+++ app/homework/page.js
@@ -1,22 +1,6 @@
-export default function HomeworkPage() {
-  return (
-    <div style={{ padding: "20px" }}>
-      <h1>Homework</h1>
+'use client';
 
-      <div style={{ marginTop: "20px" }}>
-        <h3>Solve 20 Fork Puzzles</h3>
-        <p>Due: 2026-06-21</p>
-      </div>
+import { useEffect, useState } from 'react';
+import { supabase } from '../../lib/supabaseClient';
 
-      <div style={{ marginTop: "20px" }}>
-        <h3>Study Outposts Chapter 1</h3>
-        <p>Due: 2026-06-21</p>
-      </div>
-
-      <div style={{ marginTop: "20px" }}>
-        <h3>Analyse Carlsen vs Anand</h3>
-        <p>Due: 2026-06-28</p>
-      </div>
-    </div>
-  );
-}
+export
